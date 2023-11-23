577a49dca3fbc466ce29825f61622e337fd326bc
Patricia <patriciastarck@gmail.com>
2023-11-23 19:31:39 -0300

    Adicionei o carousel

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import AuthProvider from './src/context/AuthContext';
-import Routes from './src/routes';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import AuthProvider from './src/context/AuthContext'
+import Routes from './src/routes'
 
+import { Carousel } from './src/components/Carousel'
 
 export default function App({ children }) {
   return (
     <AuthProvider>
       <Routes />
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Carousel />
+      </View>
     </AuthProvider>
   )
 }
 
-{/* tag 5.1.0 */ }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 40
+  }
+})
+
+{
+  /* tag 6.0.0 */
+}

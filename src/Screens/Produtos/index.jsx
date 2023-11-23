1387091621fbb8674125343b33fbb656e2826c3b
Yslânio victor <133903663+yslanio-victor@users.noreply.github.com>
2023-11-22 23:45:16 -0300

    Adicionando flatList

--- src/Screens/Produtos/index.jsx
+++ src/Screens/Produtos/index.jsx
@@ -1,41 +1,42 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { useContext } from 'react'
-import { AuthContext } from '../../context/AuthContext'
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, FlatList } from "react-native";
+import api from "../../Services/api";
+import { TouchableOpacity } from "react-native-web";
 
+const ListaProdutos = ({ navigation }) => {
+  const [produtos, setProdutos] = useState([]);
 
-const Produtos = () => {
+  useEffect(() => {
+    api
+      .get("/produtos")
+      .then((response) => {
+        setProdutos(response.data);
+      })
+      .catch((e) => {
+        console.log("Erro ao buscar Produto", e);
+      });
+  }, []);
 
-  const { logout } = useContext(AuthContext)
+  const detalheProduto = (produtoId) => {
+    navigation.navigate("DetalheProduto", { id: produtoId });
+  };
+
+  const renderProductsItem = ({ item }) => (
+    <TouchableOpacity onPress={() => detalheProduto(item.id)}>
+      <View>
+        <Text>{item.nome}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
-    <View style={Styles.container}>
-      <Text>Produtos</Text>
-      <TouchableOpacity style={Styles.botao}>
-        <Text style={Styles.textoBotao} onPress={logout}>Sair do App</Text>
-      </TouchableOpacity>
-    </View>
-  )
+    <FlatList
+      data={produtos}
+      renderItem={renderProductsItem}
+      keyExtractor={(item) => item.id.toString()}
+      
+    />
+  );
 };
 
-export default Produtos
-
-const Styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  botao:{
-    width: '40%',
-    height: 40,
-    backgroundColor: '#38A69D',
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  textoBotao:{
-    fontSize: 20,
-    color: '#fff'
-  },
-})
+export default ListaProdutos;

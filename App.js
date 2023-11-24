26abc57db78eee40ab6b00909c54a2342c70df67
VictorSoares-C <victorsoares.c@gmail.com>
2023-11-23 23:08:22 -0300

    Finalização da

--- App.js
+++ App.js
@@ -1,31 +1,13 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import AuthProvider from './src/context/AuthContext'
-import Routes from './src/routes'
+import AuthProvider from './src/context/AuthContext';
+import Routes from './src/routes';
 
-// import { Carousel } from './src/components/Carousel'
 
 export default function App({ children }) {
   return (
     <AuthProvider>
       <Routes />
-      {/* <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Carousel />
-      </View> */}
-    </AuthProvider>    
+    </AuthProvider>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 40
-  }
-})
-
-{
-  /* tag 6.0.0 */
-}
+{/* tag 5.1.0 */ }
